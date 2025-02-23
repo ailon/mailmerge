fd277678a91f1702341706b02ae3a1481eaa0d7c
ailon <ailon@ailon.org>
2025-02-23 10:30:57 +0200

    add config

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import { parseFile } from "@fast-csv/parse";
+import * as fs from "fs";
 import * as nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log(process.env.TGMM_SMTP_HOST);
-console.log(process.env.TGMM_SMTP_PORT);
-console.log(process.env.TGMM_SMTP_USER);
+interface Config {
+  fromEmail: string;
+  emailField: string;
+}
+
+const config: Config = JSON.parse(fs.readFileSync("tgmmconfig.json", "utf8"));
 
 parseFile("data.csv", { headers: true })
   .on("error", (error) => console.error(error))
@@ -23,15 +27,17 @@ const transporter = nodemailer.createTransport({
 });
 
 async function mail(row: any) {
-  const info = await transporter.sendMail({
-    from: process.env.TGMM_FROM,
-    to: row.Email,
-    subject: "Hello ✔",
-    text: "Hello world?", // plain text body
-    //html: "<b>Hello world?</b>", // html body
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.TGMM_FROM,
+      to: row[config.emailField],
+      subject: "Hello ✔",
+      text: "Hello world?", // plain text body
+      //html: "<b>Hello world?</b>", // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Send failed: %s", error);
+  }
 }
-
-//mail().catch(console.error);
